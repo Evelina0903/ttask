ef3d2b294072085d1bfcf42b6bf7d80bac754b61
Name <evelinaskrabkova443@gmail.com>
2024-07-27 21:38:23 +0300

    first commit

---
+++ src/app/edit/edit.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      edit works!
+    </p>
+  `,
+  styleUrl: './edit.component.css'
+})
+export class EditComponent {
+
+}
